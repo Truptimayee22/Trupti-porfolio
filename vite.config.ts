a771d5e79a591ffad000c845b54ebd1ad915dede
Truptimayee22 <truptimayeesahu62@gmail.com>
2026-05-25 22:57:30 +0530

    2nd commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,23 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import path from 'path'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       '@': path.resolve(__dirname, './src'),
+//     },
+//   },
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
+  base: '/Trupti-porfolio/',
   plugins: [react()],
   resolve: {
     alias: {
