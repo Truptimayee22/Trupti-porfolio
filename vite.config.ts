16cf24f67b013babae8fdeb8049903d0bf8c751f
Truptimayee22 <truptimayeesahu62@gmail.com>
2026-05-26 12:30:25 +0530

    new build

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,13 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import path from 'path'
-
-// export default defineConfig({
-//   plugins: [react()],
-//   resolve: {
-//     alias: {
-//       '@': path.resolve(__dirname, './src'),
-//     },
-//   },
-// })
-
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
+import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
-  base: '/Trupti-porfolio/',
+  base: process.env.NODE_ENV === 'production' ? '/Trupti-porfolio/' : '/',
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
