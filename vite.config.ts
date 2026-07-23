f2cf005a17e88221343eb39f78f8aa6369214127
Truptimayee22 <truptimayeesahu62@gmail.com>
2026-07-23 13:39:49 +0530

    dist file remove

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/Trupti-porfolio/' : '/',
+  base: process.env.GITHUB_ACTIONS === 'true' ? '/Trupti-porfolio/' : '/',
   plugins: [react()],
   resolve: {
     alias: {
